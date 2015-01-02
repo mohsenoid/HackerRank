59ce46a10dbfcda3675c8a9725a9456c3300f8a3
Mohsen <mohsen@karinaco.ir>
2015-01-02 22:25:57 +0330

    The Love-Letter Mystery

---
+++ src/main/java/com/hackerrank/algorithms/warmup/_07_TheLoveLetterMystery.java
@@ -0,0 +1,50 @@
+package com.hackerrank.algorithms.warmup; /**
+ * The Love-Letter Mystery
+ * Created by Mohsen on 12/31/14.
+ */
+
+import java.util.Scanner;
+
+public class _07_TheLoveLetterMystery {
+
+    static int count;
+    static String[] strings;
+
+
+    static int solveMeFirst(String input) {
+        int result = 0;
+
+        int len = input.length();
+        int half = len / 2;
+
+        char[] chars = input.toCharArray();
+
+        for (int i = 0; i < half; i++) {
+            if (chars[i] != chars[len - i - 1]) {
+                //while (chars[i] != chars[len - i - 1]) {
+                //chars[len - i - 1] = (char) (chars[len - i - 1] - 1);
+                result += Math.abs(chars[i] - chars[len - i - 1]);
+            }
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        count = in.nextInt();
+        strings = new String[count];
+
+        for (int i = 0; i < count; i++) {
+            strings[i] = in.next();
+        }
+
+
+        for (int i = 0; i < count; i++) {
+            System.out.println(solveMeFirst(strings[i]));
+        }
+
+    }
+}
