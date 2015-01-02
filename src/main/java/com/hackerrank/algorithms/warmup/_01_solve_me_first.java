b6beb0b08911ce78f116fe923d5f83a1e7051537
Mohsen <mohsen@karinaco.ir>
2015-01-02 22:14:34 +0330

    Solve me first

---
+++ src/main/java/com/hackerrank/algorithms/warmup/_01_solve_me_first.java
@@ -0,0 +1,33 @@
+package com.hackerrank.algorithms.warmup;
+/**
+ * Solve me first
+ * Created by Mohsen on 12/31/14.
+ */
+
+import java.util.Scanner;
+
+public class _01_solve_me_first {
+
+
+    static int solveMeFirst(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int _a;
+        _a=  in.nextInt();
+
+        int _b;
+        _b = in.nextInt();
+
+        int sum;
+        sum = solveMeFirst(_a, _b);
+
+        System.out.println(sum);
+    }
+}
+
+
+
+
