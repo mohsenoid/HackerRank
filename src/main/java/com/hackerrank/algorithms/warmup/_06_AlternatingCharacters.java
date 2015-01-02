4656d4e9db5199d4d0902719f4017a584e9eebd5
Mohsen <mohsen@karinaco.ir>
2015-01-02 22:24:58 +0330

    Alternating Characters

---
+++ src/main/java/com/hackerrank/algorithms/warmup/_06_AlternatingCharacters.java
@@ -0,0 +1,44 @@
+package com.hackerrank.algorithms.warmup; /**
+ * Alternating Characters
+ * Created by Mohsen on 12/31/14.
+ */
+
+import java.util.Scanner;
+
+public class _06_AlternatingCharacters {
+
+    static int count;
+    static String[] strings;
+
+
+    static int solveMeFirst(String input) {
+        int result = 0;
+        char last = input.charAt(0);
+        for (int i = 1; i < input.length(); i++) {
+            if (last == input.charAt(i))
+                result++;
+            else
+                last = input.charAt(i);
+        }
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        count = in.nextInt();
+        strings = new String[count];
+
+        for (int i = 0; i < count; i++) {
+            strings[i] = in.next();
+        }
+
+
+        for (int i = 0; i < count; i++) {
+            System.out.println(solveMeFirst(strings[i]));
+        }
+
+    }
+}
