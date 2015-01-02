849946f12386dbcb1814397cfd2f8e526c71d165
Mohsen <mohsen@karinaco.ir>
2015-01-02 22:36:11 +0330

    Manasa and Stones

---
+++ src/main/java/com/hackerrank/algorithms/warmup/_15_ManasaAndStones.java
@@ -0,0 +1,47 @@
+package com.hackerrank.algorithms.warmup;
+/**
+ * Manasa and Stones
+ * Created by Mohsen on 1/2/15.
+ */
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+
+public class _15_ManasaAndStones {
+    static int t;
+    static ArrayList<ArrayList<Integer>> results;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        Scanner in = new Scanner(System.in);
+
+        t = in.nextInt();
+
+        results = new ArrayList<ArrayList<Integer>>();
+
+        for (int i = 0; i < t; i++) {
+            int n = in.nextInt();
+            int a = in.nextInt();
+            int b = in.nextInt();
+
+            ArrayList<Integer> result = new ArrayList<Integer>();
+
+            if (n == 1) {
+                result.add(0);
+            } else {
+                for (int j = 0; j < n; j++) {
+                    result.add(a * j + b * (n - 1 - j));
+                }
+                Collections.sort(result);
+            }
+
+            results.add(result);
+        }
+
+        for (int i = 0; i < t; i++) {
+            System.out.println(results.get(i).toString().replace("[", "").replace("]","").replace(",",""));
+        }
+    }
+}
