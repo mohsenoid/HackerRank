c2a9914cee44ce59cfbd6e254e759355f8b6171a
Mohsen <mohsen@karinaco.ir>
2015-01-02 22:18:44 +0330

    Is Fibo

---
+++ src/main/java/com/hackerrank/algorithms/warmup/_03_IsFibo.java
@@ -0,0 +1,62 @@
+package com.hackerrank.algorithms.warmup; /**
+ * Is Fibo
+ * Created by Mohsen on 12/31/14.
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class _03_IsFibo {
+
+    static int count;
+    static long[] numbers;
+
+    static ArrayList<Long> fibo;
+
+    enum FIBO {
+        IsFibo,
+        IsNotFibo
+    }
+
+    static FIBO solveFibo(long number) {
+        if (fibo.get(fibo.size()-1) < number)
+            updateFibo(number);
+
+        if (fibo.contains(number))
+            return FIBO.IsFibo;
+        else
+            return FIBO.IsNotFibo;
+    }
+
+    private static void updateFibo(long number) {
+        int last = fibo.size()-1;
+        long lastNumber = fibo.get(last);
+
+        do {
+            lastNumber = fibo.get(last) + fibo.get(last - 1);
+            fibo.add(lastNumber);
+            last++;
+        } while (lastNumber < number);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        count = in.nextInt();
+        numbers = new long[count];
+
+        for (int i = 0; i < count; i++) {
+            numbers[i] = in.nextLong();
+        }
+
+        fibo = new ArrayList<Long>();
+        fibo.add(0l);
+        fibo.add(1l);
+
+
+        for (int i = 0; i < count; i++) {
+            System.out.println(solveFibo(numbers[i]));
+        }
+
+    }
+}
