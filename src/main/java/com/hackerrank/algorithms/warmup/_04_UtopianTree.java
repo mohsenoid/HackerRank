37dea62eea1f4e981a3f2bba6d24f25e81d84f7e
Mohsen <mohsen@karinaco.ir>
2015-01-02 22:20:14 +0330

    Utopian Tree

---
+++ src/main/java/com/hackerrank/algorithms/warmup/_04_UtopianTree.java
@@ -0,0 +1,44 @@
+package com.hackerrank.algorithms.warmup; /**
+ * Utopian Tree
+ * Created by Mohsen on 12/31/14.
+ */
+
+import java.util.Scanner;
+
+public class _04_UtopianTree {
+
+    static int count;
+    static long[] numbers;
+
+
+    static int solveMeFirst(long number) {
+        int result =1;
+        for(int i=0;i<number/2;i++){
+            result = result*2+1;
+        }
+        if(number%2!=0)
+            result=result*2;
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        count = in.nextInt();
+        numbers = new long[count];
+
+        for (int i = 0; i < count; i++) {
+            numbers[i] = in.nextLong();
+        }
+
+
+
+
+        for (int i = 0; i < count; i++) {
+            System.out.println(solveMeFirst(numbers[i]));
+        }
+
+    }
+}
