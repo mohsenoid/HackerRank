4ed86e99631a30beca126c657b310d9abad095ef
Mohsen <mohsen@karinaco.ir>
2015-01-10 15:32:48 +0330

    Flipping bits

---
+++ src/main/java/com/hackerrank/algorithms/warmup/FlippingBits.java
@@ -0,0 +1,39 @@
+package com.hackerrank.algorithms.warmup;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by Mohsen on 1/10/15.
+ */
+public class FlippingBits {
+    static int t;
+    static long[] results;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        Scanner in = new Scanner(System.in);
+
+        t = in.nextInt();
+
+        results = new long[t];
+
+        for (int i = 0; i < t; i++) {
+            long n = in.nextLong();
+
+            results[i] = flipBits(n);
+
+
+        }
+        for (int i = 0; i < t; i++) {
+            System.out.println(results[i]);
+        }
+    }
+
+    private static long flipBits(long n) {
+        long mask = 0xffffffffl;
+        long result = n ^ mask;
+        return result;
+    }
+
+
+}
