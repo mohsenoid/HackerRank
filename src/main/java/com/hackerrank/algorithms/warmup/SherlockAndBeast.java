1e30b9582db0848f93d07f4f510ef870e9b8eebc
Mohsen <mohsen@karinaco.ir>
2015-01-12 20:00:29 +0330

    Sherlock and The Beast

--- src/main/java/com/hackerrank/algorithms/warmup/SherlockAndBeast.java
+++ src/main/java/com/hackerrank/algorithms/warmup/SherlockAndBeast.java
@@ -12,52 +12,47 @@ import java.util.Scanner;
 
 public class SherlockAndBeast {
     static int t;
-    static String[] results;
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         Scanner in = new Scanner(System.in);
 
         t = in.nextInt();
 
-        results = new String[t];
-
-        boolean flag = false;
-
         for (int i = 0; i < t; i++) {
             int n = in.nextInt();
 
-            int n5 = n / 3;
-            int n3 = n - n5 * 3;
-            while (n5 > 0 && n3 > 0 && n3 % 5 != 0) {
-                n5--;
-                n3 = n - n5 * 3;
+            int m3 = n / 3;
+            int r3 = n % 3;
 
-            }
-            if (n3 % 5 == 0) {
-                flag = true;
+            int m5 = r3 / 5;
+            int r5 = r3 % 5;
+            while (r5 != 0 && m3 > 0) {
+                r3 += 3;
+                m3--;
+
+                m5 = r3 / 5;
+                r5 = r3 % 5;
             }
 
             String result = "";
 
-            if (flag == false) {
+            if (r5 != 0) {
                 result = "-1";
             } else {
-                //int n3 = (int) n3_d;
-                while (n5 > 0) {
+                for (int j = 0; j < m3; j++) {
                     result += "555";
-                    n5--;
                 }
-                while (n3 > 0) {
-                    result += "3";
-                    n3--;
+
+                for (int j = 0; j < m5; j++) {
+                    result += "33333";
                 }
             }
 
-            results[i] = result;
-        }
-        for (int i = 0; i < t; i++) {
-            System.out.println(String.valueOf(results[i]));
+            System.out.println(result);
+
         }
 
     }
+
+
 }
